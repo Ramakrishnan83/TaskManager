654a70ade499eb2de1dcdb27b5831d49b7bc87c8
Ramakrishnan83 <45376705+Ramakrishnan83@users.noreply.github.com>
2018-12-10 22:43:25 -0500

    Add files via upload

--- app/db/database.js
+++ app/db/database.js
@@ -1,7 +1,7 @@
 var mongoose=require('mongoose');
 //'mongodb://localhost:27017/task_manager'
 
-mongoose.connect('mongodb://ramdemo:rahul1402@ds163630.mlab.com:63630/task_manager', { useNewUrlParser: true }, function(err){
+mongoose.connect('mongodb://ramtask:Rahul1402@ds163630.mlab.com:63630/task_manager', { useNewUrlParser: true }, function(err){
     if(!!err){
         console.log("not able to connection");
         console.error(err);
